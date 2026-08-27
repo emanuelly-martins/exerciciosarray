7ee6514feaceb15c0af9627dc6af49361134ac0e
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-08-27 11:43:38 -0300

    procurando uma fruta

---
+++ Nivel01/exercicio01.js
@@ -0,0 +1,5 @@
+// 🟢 Nível 1 — includes() e indexOf()
+// 1. Procurando uma fruta
+
+const frutas = ["Banana", "Abacaxi", "Morango", "Uva", "Melancia"];
+console.log ("Tem Banana?", frutas.includes("Banana"));
