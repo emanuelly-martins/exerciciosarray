995139dcdc6a00600c584f79732f879fb7930b60
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-08-27 11:51:33 -0300

    encontrando posição

---
+++ Nivel01/exercicio03.js
@@ -0,0 +1,5 @@
+// 🟢 Nível 1 — includes() e indexOf()
+// 3. Encontrando a posição
+
+const nomes = ["Emanuelly", "Letícia", "Maria Eduarda", "Gabriela", "Pamela", "Paloma"];
+console.log ("Posição de Gabriela:", nomes.indexOf("Gabriela"));
