5b4183c8af46bea8c8ebae0a013b0c6253893584
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-08-27 11:47:31 -0300

    verificando uma linguagem

---
+++ Nivel01/exercicio02.js
@@ -0,0 +1,7 @@
+// 🟢 Nível 1 — includes() e indexOf()
+// 2. Verificando uma linguagem
+
+let linguagens = ["JavaScript", "Python", "Java", "C#", "PHP"];
+console.log ("Tem JavaScript?", linguagens.includes("JavaScript"));
+console.log ("Tem Python?", linguagens.includes("Python"));
+console.log ("Tem C++?", linguagens.includes("C++"));
