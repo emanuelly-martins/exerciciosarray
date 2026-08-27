ab22877afd9fbcf2728c5cd1b704b7f5bf396eb5
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-08-27 11:56:51 -0300

    aluno não encontrado

---
+++ Nivel01/exercicio04.js
@@ -0,0 +1,8 @@
+// 🟢 Nível 1 — includes() e indexOf()
+// 4. Aluno não encontrado
+
+const nomes = ["Emanuelly", "Letícia", "Maria Eduarda", "Gabriela", "Pamela", "Paloma"];
+console.log ("Posição de Kevin:", nomes.indexOf("Kevin"));
+
+// 🗒️ Explicação:
+// Como o nome "Kevin" não existe no array, o JavaScript irá retornar -1, porque o elemento não foi encontrado.
