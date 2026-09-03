1b0969f9f5f62f554245340784533ea17c5f44b2
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-03 13:49:39 -0300

    removendo vários elementos

---
+++ Nivel03/exercicio10.js
@@ -0,0 +1,15 @@
+// 🟠 Nível 3 — splice()
+// 10. Removendo vários elementos
+
+const nomes = [
+    'Emanuelly',
+    'Letícia',
+    'Maria Eduarda',
+    'Gabriela',
+    'Pamela',
+    'Paloma',
+    'Anabelle',
+];
+
+nomes.splice(3, 3);
+console.log(nomes);
