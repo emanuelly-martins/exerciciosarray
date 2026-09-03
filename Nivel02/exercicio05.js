9190d8937a715641473b62c29107545bd8559208
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-03 13:26:22 -0300

    copiando parte do array

--- Nivel02/exercicio05.js
+++ Nivel02/exercicio05.js
@@ -1,3 +1,6 @@
+// 🟡 Nível 2 — slice()
+// 5. Copiando parte do array
+
 let frutas = ['Maçã', 'Banana', 'Laranja', 'Uva', 'Manga'];
 let novasFrutas = frutas.slice(1, 4);
 
---
+++ Nivel02/exercicio06.js
@@ -0,0 +1,4 @@
+const nomes = ["Emanuelly", "Letícia", "Maria Eduarda", "Gabriela", "Pamela", "Paloma", "Anabelle", "Kevin"];
+const novosNomes = nomes.slice(0, 3);
+
+console.log(novosNomes);
