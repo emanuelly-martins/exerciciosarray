ec9220d52c4716a637c06d2441f1550c9b11f0ad
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-03 13:26:51 -0300

    primeiros elementos

--- Nivel02/exercicio06.js
+++ Nivel02/exercicio06.js
@@ -1,4 +1,16 @@
-const nomes = ["Emanuelly", "Letícia", "Maria Eduarda", "Gabriela", "Pamela", "Paloma", "Anabelle", "Kevin"];
+// 🟡 Nível 2 — slice()
+// 6. Primeiros elementos
+
+const nomes = [
+    'Emanuelly',
+    'Letícia',
+    'Maria Eduarda',
+    'Gabriela',
+    'Pamela',
+    'Paloma',
+    'Anabelle',
+    'Kevin',
+];
 const novosNomes = nomes.slice(0, 3);
 
 console.log(novosNomes);
