fc502975533135e0473f261eb549bee7141b72cb
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-03 14:45:32 -0300

    organizando uma lista

---
+++ Nivel06/exercicio21.js
@@ -0,0 +1,27 @@
+// 🔴 Nível 6 — Combinando métodos
+// 21. Organizando uma lista
+
+const nomes = [
+    'Emanuelly',
+    'Letícia',
+    'Maria Eduarda',
+    'Gabriela',
+    'Pamela',
+    'Paloma',
+    'Anabelle',
+    'Kevin',
+];
+
+// 1. original
+console.log(nomes);
+
+// 2. sort()
+nomes.sort();
+console.log(nomes);
+
+// 3. reverse()
+nomes.reverse();
+console.log(nomes);
+
+// 4. join()
+console.log(nomes.join(' '));
