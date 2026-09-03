6b5177ab9ce81568a056219aa506a2c65927f549
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-03 14:09:18 -0300

    invertendo uma lista

---
+++ Nivel05/exercicio16.js
@@ -0,0 +1,6 @@
+ // 🟣 Nível 5 — reverse() e sort()
+// 16. Invertendo uma lista
+
+const nomes = ['Emanuelly', 'Letícia', 'Maria Eduarda', 'Gabriela', 'Pamela'];
+nomes.reverse();
+console.log(nomes);
