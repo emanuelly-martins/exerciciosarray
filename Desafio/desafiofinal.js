3a70ed8b2c42db61a189280f005a4ae7a4b13639
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-03 15:56:52 -0300

    Desafio Final - Sistema de Lista

--- Desafio/desafiofinal.js
+++ Desafio/desafiofinal.js
@@ -16,34 +16,34 @@ console.log('1 - Aluno existe?');
 console.log(alunos.includes('Emanuelly'));
 
 // 2 - Encontrar posição de um aluno
-console.log('\n2 - Posição do aluno:');
+console.log('Posição do aluno:');
 console.log(alunos.indexOf('Letícia'));
 
 // 3 - Criar uma parte da lista
-console.log('\n3 - Parte da lista:');
+console.log('Parte da lista:');
 let parteLista = alunos.slice(2, 5);
 console.log(parteLista);
 
 // 4 - Remover aluno
-console.log('\n4 - Removendo aluno:');
+console.log('Removendo aluno:');
 alunos.splice(3, 1);
 console.log(alunos);
 
 // 5 - Inserir aluno
-console.log('\n5 - Inserindo aluno:');
+console.log('Inserindo aluno:');
 alunos.splice(2, 0, 'Pamela');
 console.log(alunos);
 
 // 6 - Ordenar alunos
-console.log('\n6 - Lista ordenada:');
+console.log('Lista ordenada:');
 alunos.sort();
 console.log(alunos);
 
 // 7 - Inverter lista
-console.log('\n7 - Lista invertida:');
+console.log('Lista invertida:');
 alunos.reverse();
 console.log(alunos);
 
 // 8 - Exibir lista como texto
-console.log('\n8 - Lista como texto:');
+console.log('Lista como texto:');
 console.log(alunos.join(', '));
