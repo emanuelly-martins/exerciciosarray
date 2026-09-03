9ad214770edae90233b796199d9fd3864e7fa209
emanuelly-martins <emanuelly.ramos@edu.senai.br>
2026-09-03 14:15:47 -0300

    ordenando nomes

---
+++ Nivel05/exercicio18.js
@@ -0,0 +1,15 @@
+// 🟣 Nível 5 — reverse() e sort()
+// 18. Ordenando nomes
+
+const nomes = [
+    'Emanuelly',
+    'Letícia',
+    'Maria Eduarda',
+    'Gabriela',
+    'Pamela',
+    'Paloma',
+    'Anabelle',
+    'Kevin',
+];
+nomes.sort();
+console.log(nomes);
